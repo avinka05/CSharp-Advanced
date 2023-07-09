a1d79071ae275851a08be52be1886312ecfd5b19
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-07-09 16:21:23 +0300

    1. Reverse a String

---
+++ Stacks and Queues - Lab/1. Reverse a String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Reverse_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> text = new Stack<char>(input);
+
+            while (text.Any())
+            {
+                char result = text.Pop();
+                Console.Write(result);
+            }
+
+        }
+    }
+}
