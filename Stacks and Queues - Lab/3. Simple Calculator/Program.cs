39ab6604843a2114fba6f04a509ac4491c13a3df
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-07-12 09:55:04 +0300

    3. Simple Calculator

---
+++ Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = Console.ReadLine().Split().ToArray();
+            
+            
+            Stack<string> expression = new Stack<string>(inputArray.Reverse());
+
+            int result = int.Parse(expression.Pop());
+
+            while (expression.Count>0)
+            {
+                string operation = expression.Pop();
+
+                int number = int.Parse(expression.Pop());
+
+                if (operation=="+")
+                {
+                    result += number;
+                }
+                else if (operation == "-")
+                {
+                    result -= number;
+                }
+
+            }
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
