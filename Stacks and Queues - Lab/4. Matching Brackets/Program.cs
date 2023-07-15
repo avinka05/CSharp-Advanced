90d3da77f26cf75a71b789aaf70043c6f444893f
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-07-15 20:31:40 +0300

    Create Program.cs

---
+++ Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<int> indexes = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)           
+            {
+                if (expression[i]=='(')
+                {
+                    indexes.Push(i);
+                    continue;
+                }
+                else if (expression[i] == ')')
+                {
+                    int opened = indexes.Pop();
+                    for (int j = opened; j <= i; j++)
+                    {
+                        Console.Write(expression[j]);
+                    }
+                    Console.WriteLine();
+                }
+
+
+            }
+
+        }
+    }
+}
