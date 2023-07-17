9217f64f4f0a8acde0becfdbc13fc2c13834a9a0
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-07-17 12:17:05 +0300

    5. Print Even Numbers

---
+++ Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> numbersQueue = new Queue<int>(array);
+
+            List<int> evenNumbers = new List<int>();
+
+            while (numbersQueue.Count > 0)
+            {
+                int number = numbersQueue.Dequeue();
+
+                if (number % 2 == 0)
+                {
+                    evenNumbers.Add(number);
+                }
+            }
+
+            string evenNumbersString = string.Join(", ", evenNumbers);
+            Console.WriteLine(evenNumbersString);
+        }
+    }
+}
