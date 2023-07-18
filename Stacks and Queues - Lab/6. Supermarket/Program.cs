3ca74555c4d2304b194d2d9a9026dee86830b852
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-07-18 19:42:02 +0300

    6. Supermarket

---
+++ Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Queue<string> names = new Queue<string>();
+
+            while (input!="End")
+            {
+
+                if (input=="Paid")
+                {
+                    int count = names.Count;
+                    for (int i = 0; i < count; i++)
+                    {
+                        string name = names.Dequeue();
+                        Console.WriteLine(name);
+                    }
+                }
+                else
+                {
+                    names.Enqueue(input);
+                }
+
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
