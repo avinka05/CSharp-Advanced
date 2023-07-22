7a5cfc05ade09d1ee01933c305e6c44350e14f7e
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-07-22 20:08:07 +0300

    Create Program.cs

---
+++ Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+
+            string input;
+            int counter = 0;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    if (cars.Count <= n)
+                    {
+                        n = cars.Count;
+                    }
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        counter++;
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
