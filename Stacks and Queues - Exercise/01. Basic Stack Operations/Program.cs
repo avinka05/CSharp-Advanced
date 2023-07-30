692381041479a7fbf8bb97fb545d7268b22e0146
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-07-30 21:18:47 +0300

    Create Program.cs

---
+++ Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int n = numbers[0];
+int s = numbers[1];
+int x = numbers[2];
+
+int[] numbersOfStack = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+Stack<int> stack = new Stack<int>(numbersOfStack);
+
+while (s>0&&stack.Any())
+{
+    stack.Pop();
+    s--;
+}
+
+if(!stack.Any())
+{
+    Console.WriteLine(0)  ;
+    return;
+}
+
+if (stack.Contains(x))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine(stack.Min()) ;
+}
+
+
+
+
+
